5e38af51e4361806b929288e4744dce562e3c716
fein-denscothmn <denscothmn@gmail.com>
2025-03-08 17:19:11 +0900

    fein domain

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = 'fein-cache-' + new Date().getTime(); // キャッシュ名を現在のタイムスタンプで更新
+const CACHE_NAME = 'fein-cache-' + new Date().getTime();
 
 const urlsToCache = [
  '/index.html'
 ];
 
-// インストールイベント
 self.addEventListener('install', event => {
  event.waitUntil(
   caches.open(CACHE_NAME)
@@ -12,16 +11,15 @@ self.addEventListener('install', event => {
     return cache.addAll(urlsToCache);
    })
  );
- self.skipWaiting(); // 新しいサービスワーカーを即座に制御させる
+ self.skipWaiting();
 });
 
-// アクティブ化イベント
 self.addEventListener('activate', event => {
  event.waitUntil(
   caches.keys().then(cacheNames => {
    return Promise.all(
     cacheNames.filter(function (cacheName) {
-     // 現在のキャッシュ名と一致しないキャッシュを削除
+
      return cacheName.startsWith('fein-cache-') && cacheName !== CACHE_NAME;
     }).map(function (cacheName) {
      return caches.delete(cacheName);
@@ -29,28 +27,27 @@ self.addEventListener('activate', event => {
    );
   })
  );
- self.clients.claim(); // クライアントへの制御を即座に開始
+ self.clients.claim();
 });
 
-// フェッチイベント
 self.addEventListener('fetch', event => {
  if (event.request.mode === 'navigate') {
-  // ページナビゲーションの場合はネットワーク優先
+
   event.respondWith(
    fetch(event.request)
     .then(response => {
      const clone = response.clone();
      caches.open(CACHE_NAME).then(cache => {
-      cache.put(event.request, clone); // 最新のレスポンスをキャッシュに保存
+      cache.put(event.request, clone);
      });
      return response;
     })
     .catch(() => {
-     return caches.match(event.request); // オフライン時はキャッシュから取得
+     return caches.match(event.request);
     })
   );
  } else {
-  // その他のリクエストはキャッシュ優先
+
   event.respondWith(
    caches.match(event.request)
     .then(response => {
