bbcecd6680633841813c0a6a22ac97817d89b755
fein-denscothmn <denscothmn@gmail.com>
2025-11-30 08:00:48 +0900

    fein_ff

--- www/script/copy.js
+++ www/script/copy.js
@@ -1,9 +1,9 @@
 document.getElementById('copyText').addEventListener('click', function () {
  const text = this.textContent;
  navigator.clipboard.writeText(text).then(() => {
-  alert('招待コードをコピーしました！');
+  alert('招待コードをコピーしました。『アナザーエデン 時空を超える猫』×『FINAL FANTASY IX』コラボを楽しんでくださいね♪');
  }).catch(err => {
-  alert('招待コードのコピーに失敗しました。申し訳ございませんが、スマホのデフォルト機能を使ってコピー願います。');
+  alert('招待コードのコピーに失敗しました。申し訳ございませんが、お使いのモバイルデバイスの機能を使ってコピー願います。');
   console.error(err);
  });
 });
